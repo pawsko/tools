7a4b94e54f9805f8cc6b98ce8167497c49cb276f
Pawel Skora <pawsko.to@gmail.com>
2022-08-08 17:21:23 +0200

    Tool entity set

--- src/main/java/pl/coderslab/tools/tool/Tool.java
+++ src/main/java/pl/coderslab/tools/tool/Tool.java
@@ -1,2 +1,25 @@
-package pl.coderslab.tools.tool;public class Tool {
+package pl.coderslab.tools.tool;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "tools")
+public class Tool {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String model;
+    private String manufacturer;
+    private String category;
+    private String type;  //electro, handy
+    private String powerType; //if applicable 230, battery
+    private String status; //Available, not available, damaged, in service
+    private int rating; //included accuracy, rate of wear and tear
+
 }
