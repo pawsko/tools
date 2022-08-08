c0e9d568e291042f6bd553d7ae76810e10ec733d
Pawel Skora <pawsko.to@gmail.com>
2022-08-08 17:18:11 +0200

    Rental entity set

--- src/main/java/pl/coderslab/tools/rental/Rental.java
+++ src/main/java/pl/coderslab/tools/rental/Rental.java
@@ -1,2 +1,28 @@
-package pl.coderslab.tools.rental;public class Rental {
+package pl.coderslab.tools.rental;
+
+import lombok.Getter;
+import lombok.Setter;
+import pl.coderslab.tools.tool.Tool;
+import pl.coderslab.tools.user.User;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "rentals")
+public class Rental {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime rented;
+    private LocalDateTime returned;
+    private String notices;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Tool tool;
 }
